9d76943a5b1f479dbb09df6993505ffd3831b2f4
jesusb25 <jesusballesteros2500@gmail.com>
2023-08-01 16:07:54 -0700

    missed commit

--- server.js
+++ server.js
@@ -1,13 +1,19 @@
 const express = require('express');
 const cors = require('cors');
 const app = express();
-const gtts = require('node-gtts')('en');
-var path = require('path');
 const PORT = 8000
 const googleTTS = require('google-tts-api');
+const fileUpload = require('express-fileupload');
+const pdfjsLib = require('pdfjs-dist');
 
+app.use(fileUpload());
 app.use( express.json() );
 app.use(cors());
+app.listen(
+  PORT,
+  () => console.log(`it's alive on http://localhost:${PORT}`)
+);
+
 
 app.get('/base64data', (request, response) => {
   const text = request.query.text;
@@ -31,30 +37,25 @@ app.get('/base64data', (request, response) => {
 });
 
 
-app.get('/textToSpeech', (request, response) => {
-  const url = googleTTS.getAllAudioUrls(request.query.text, {
-    lang: 'en',
-    slow: false,
-    host: 'https://translate.google.com'
-  });
-  console.log(url);
-
-  const buffer = Buffer.from(url, 'utf8');
-  fs.writeFileSync('new.mp3', buffer);
-
-});
-
-const fs = require('fs');
-
-app.get('/speech', (request, response) => {
-  gtts.save("new.mp3", request.query.text, function () {
-    console.log("Text to speech converted!");
-    response.satus = 200;
-    response.send("Audio file generated!"); // You can send a response back to the client if needed.
+app.post('/get-text', (request, response) => {
+  if (!request.files && !request.files.pdf) {
+    response.status(400).send('No files were uploaded.');
+    return;
+  }
+  const src = request.files.pdf;
+  getText(src).then(text => {
+    response.send(text);
   });
 });
 
-app.listen(
-  PORT,
-  () => console.log(`it's alive on http://localhost:${PORT}`)
-);
+async function getContent(src) {
+  const doc = await pdfjsLib.getDocument(src).promise;
+  const page = await doc.getPage(1);
+  return await page.getTextContent();
+}
+
+async function getText(src) {
+  const content = await getContent(src);
+  const items = content.items.map(item => item.str);
+  return items.join(' ');
+}
